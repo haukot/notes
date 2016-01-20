ad3d3bf984b6d7d18c82f791d05dc0a15301a4c7
Dmitry Davydov <haudvd@gmail.com>
2016-01-21 01:22:12 +0300

    replace expandNoteChildren by notesIterator

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,4 +1,29 @@
 import {fromJS} from 'immutable';
+
+function notesIterator(state, note, counter, acc, noteCallback, childCallback) {
+    let newCounter = counter;
+    const res = noteCallback(note, counter, acc);
+    let newAcc = res.acc;
+    let newNote = res.note
+    if (!newNote.get('children') || newNote.get('children').count() <= 0) {
+        return {counter: newCounter, acc: newAcc, note: newNote};
+    }
+    newNote = newNote
+          .update('children', children =>
+                  children
+                  .map((childId, id) => {
+                      let child = state.getIn(['notes', childId])
+                      child = childCallback(child, children, id, newCounter, acc); // callback
+                      const expand = notesIterator(state, child, newCounter + 1,
+                                                   newAcc, noteCallback, childCallback);
+                      newCounter = expand.counter;
+                      newAcc = Object.assign(newAcc, expand.acc);
+                      return expand.note;
+                  })
+                 );
+    return {counter: newCounter, note: newNote, acc: newAcc};
+}
+
 // global order должен быть отдельной функцией или индексом в стейте,
 // соответствием order: note
 function globalOrder0(state, note, counter, acc) {
@@ -31,33 +56,21 @@ export function globalOrder(state) {
     console.log("acc", fromJS(acc).toJS());
     return fromJS(acc);
 }
-// TODO мб сделать функцию, которая будет global order отдельно навешивать
-function expandNoteChildren(note, state, counter) {
-    let newCounter = counter;
-    // console.log(note.get("id"), newCounter);
-    if (note.get('children').count() <= 0) return {counter: newCounter,
-                                                   note: note.set('globalOrder', newCounter)};
-    const newNote = note
-          .set('globalOrder', newCounter)
-          .update('children', children =>
-                  children
-                  .map((childId, id) => {
-                      newCounter = newCounter + 1;
-                      let childNote = state.getIn(['notes', childId])
-                          .set('order', id)
-                          .set('prevId', children.get(id - 1));
-                      const expand = expandNoteChildren(childNote, state, newCounter);
-                      newCounter = expand.counter;
-                      // console.log("map", childNote.get("id"), newCounter);
-                      return expand.note;
-                  })
-                  .sortBy(note => note.get('order'))
-                 );
-    return {counter: newCounter, note: newNote};
-}
 
 export function notes(state) {
-    let {counter, note} = expandNoteChildren(state.getIn(['notes', 0]), state, 0);
+    // let {counter, note} = expandNoteChildren(state.getIn(['notes', 0]), state, 0);
+    let childCallback = (child, children, id, counter, acc) => {
+        let newChild = child
+            .set('order', id)
+            .set('prevId', children.get(id - 1));
+        return newChild;
+    };
+    let noteCallback = (note, counter, acc) => {
+        const newNote = note.set('globalOrder', counter);
+        return {note: newNote, acc}
+    };
+    let {counter, note} = notesIterator(state, state.getIn(['notes', 0]), 0, {},
+                                        noteCallback, childCallback);
     console.log("notes", note.toJS());
     return note;
 }
